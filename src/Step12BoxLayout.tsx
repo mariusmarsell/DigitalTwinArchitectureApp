b24bb820ab6d6fd42e74740ea23fca434629d579
mariusmarsell <marius.marsell@outlook.de>
2025-07-09 15:25:45 +0100

    Update Step12BoxLayout.tsx

--- src/Step12BoxLayout.tsx
+++ src/Step12BoxLayout.tsx
@@ -1,66 +1,92 @@
-export default function DigitalTwinBox({
-  title,
-  primaryItems,
-  subtitle,
-  secondaryItems,
-  thirdTitle,
-  thirdItems,
-}: {
-  title: string;
-  primaryItems: string[];
-  subtitle?: string;
-  secondaryItems?: string[];
-  thirdTitle?: string;
-  thirdItems?: string[];
-}) {
-  return (
-    <div className="digital-twin-box">
-      <h3 className="section-heading">{title}</h3>
+import React from 'react';
 
-      {/* Primary items */}
-      <div className="item-list">
-        {primaryItems.length > 0 ? (
-          primaryItems.map((item, i) => (
-            <div key={i} className="item-chip">
-              {item}
-            </div>
-          ))
-        ) : (
-          <div className="no-items">No items</div>
+const DigitalTwinBox = React.forwardRef<
+  HTMLDivElement,
+  {
+    header?: string;
+    title: string;
+    primaryItems?: string[];
+    onPrimaryItemClick?: (item: string) => void;
+    subtitle?: string;
+    secondaryItems?: string[];
+    onSecondaryItemClick?: (item: string) => void;
+    thirdTitle?: string;
+    thirdItems?: string[];
+    onThirdItemClick?: (item: string) => void;
+  }
+>(
+  (
+    {
+      header,
+      title,
+      primaryItems,
+      onPrimaryItemClick,
+      subtitle,
+      secondaryItems,
+      onSecondaryItemClick,
+      thirdTitle,
+      thirdItems,
+      onThirdItemClick,
+    },
+    ref
+  ) => {
+    return (
+      <div className="digital-twin-box" ref={ref}>
+        {header && <h2 className="box-header">{header}</h2>}
+        <h3 className="section-heading">{title}</h3>
+
+        {primaryItems && primaryItems?.length > 0 && (
+          <div className="item-list">
+            {primaryItems.map((item, i) => (
+              <button
+                key={i}
+                className="item-chip"
+                onClick={() => onPrimaryItemClick?.(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {subtitle && secondaryItems && secondaryItems?.length > 0 && (
+          <p className="section-heading">{subtitle}</p>
+        )}
+
+        {secondaryItems && secondaryItems?.length > 0 && (
+          <div className="item-list">
+            {secondaryItems.map((item, i) => (
+              <button
+                key={i}
+                className="item-chip"
+                onClick={() => onSecondaryItemClick?.(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {thirdTitle && thirdItems && thirdItems?.length > 0 && (
+          <p className="section-heading">{thirdTitle}</p>
+        )}
+
+        {thirdItems && thirdItems?.length > 0 && (
+          <div className="item-list">
+            {thirdItems.map((item, i) => (
+              <button
+                key={i}
+                className="item-chip"
+                onClick={() => onThirdItemClick?.(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </div>
         )}
       </div>
+    );
+  }
+);
 
-      {/* Subtitle in between primary and secondary */}
-      {subtitle && secondaryItems && secondaryItems.length > 0 && (
-        <p className="section-heading">{subtitle}</p>
-      )}
-
-      {/* Secondary items */}
-      {secondaryItems && secondaryItems.length > 0 && (
-        <div className="item-list">
-          {secondaryItems.map((item, i) => (
-            <div key={i} className="item-chip">
-              {item}
-            </div>
-          ))}
-        </div>
-      )}
-
-      {/* Subtitle in between secondary and third */}
-      {thirdTitle && thirdItems && thirdItems.length > 0 && (
-        <p className="section-heading">{thirdTitle}</p>
-      )}
-
-      {/* Third items */}
-      {thirdItems && thirdItems.length > 0 && (
-        <div className="item-list">
-          {thirdItems.map((item, i) => (
-            <div key={i} className="item-chip">
-              {item}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
+export default DigitalTwinBox;
